12acaa451c3c97576f25484b82527f77f0ff7993
Nitin Singh <nitinsingh@Nitins-MacBook-Air.local>
2021-07-22 13:04:36 +0530

    spanish number starting phase

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text> Hello</Text>
+    </>
+  );
+};
+
+export default App;
